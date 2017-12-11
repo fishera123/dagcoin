c24bbf23a2eabdb2267ebea8c27c45b7defd7032
lasha <lasha.tsintsabadze@ignite.ee>
2017-12-11 21:19:31 +0200

    changes backup popup description

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -1397,7 +1397,8 @@ no-nested-ternary,no-shadow,no-plusplus,consistent-return,import/no-extraneous-d
         const ModalInstanceCtrl = function ($scopeModal, $modalInstance, $sce) {
           $scopeModal.header = $sce.trustAsHtml(gettextCatalog.getString('Bakup needed'));
           $scopeModal.title = $sce.trustAsHtml(gettextCatalog.getString(`Now is a good time to backup your wallet seed.
-          If this device is lost, it is impossible to access your funds without a backup.`));
+          Write it down and keep it somewhere safe. Once you have written your wallet seed down, you must delete it from
+          this device. If this device is lost, it is impossible to access your funds without a backup.`));
 
           $scopeModal.yes_label = gettextCatalog.getString('Backup now');
           $scopeModal.ok = function () {
