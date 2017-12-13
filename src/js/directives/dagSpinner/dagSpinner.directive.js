4b88e08af50a325a00c752edeafe7831fdc154f4
Roman Shabanov <gettingold@mail.ru>
2017-12-13 14:08:29 +0200

    Transactions table

--- src/js/directives/dagSpinner/dagSpinner.directive.js
+++ src/js/directives/dagSpinner/dagSpinner.directive.js
@@ -17,21 +17,7 @@
       scope: {
         color: '@'
       },
-      template: '<div ng-style="color && {\'color\': color}">' +
-      '<div class="sk-fading-circle">' +
-      '<div class="sk-circle1 sk-circle"></div>' +
-      '<div class="sk-circle2 sk-circle"></div>' +
-      '<div class="sk-circle3 sk-circle"></div>' +
-      '<div class="sk-circle4 sk-circle"></div>' +
-      '<div class="sk-circle5 sk-circle"></div>' +
-      '<div class="sk-circle6 sk-circle"></div>' +
-      '<div class="sk-circle7 sk-circle"></div>' +
-      '<div class="sk-circle8 sk-circle"></div>' +
-      '<div class="sk-circle9 sk-circle"></div>' +
-      '<div class="sk-circle10 sk-circle"></div>' +
-      '<div class="sk-circle11 sk-circle"></div>' +
-      '<div class="sk-circle12 sk-circle"></div>' +
-      '</div></div>',
+      template: '<div class="loader"></div>',
       link: () => {
       }
     };
